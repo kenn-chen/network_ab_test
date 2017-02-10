1aad4727854dea19eb11d91039008ef3e5b83b80
Kenn <kenn.kikyoakane@gmail.com>
2017-02-10 19:03:48 +0900

    ...

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ if [ ! -f logs/error.log ]; then
 	touch 'logs/error.log'
 fi
 
-methods=('baseline1' 'baseline2', 'baseline3')
+methods=('baseline1' 'baseline2' 'baseline3')
 models=('uniform' 'linear1')
 graphs=('-g scale_free' '-g wiki-Vote' '-g soc-Epinions1' '-g soc-Slashdot0811')
 for M in $methods
