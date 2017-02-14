9ab9d93e4035ec3d313750b0e356f59d60c6975f
Kenn <kenn.kikyoakane@gmail.com>
2017-02-14 19:25:20 +0900

    ...

--- run.sh
+++ run.sh
@@ -19,20 +19,17 @@ fi
 lambda1=(0 0.25 0.75 1)
 lambda2=(0 0.1 0.5 1)
 methods=('baseline1' 'baseline2' 'baseline3' 'new')
-models=('uniform' 'linear1')
 graphs=('growing_network' 'wiki-Vote' 'soc-Epinions1' 'soc-Slashdot0811')
 for l1 in $lambda1; do
 	for l2 in $lambda2; do
 
 		for M in $methods; do
-			for m in $models; do
-				for g in $graphs; do
-					eval "./main.py -l1 $l1 -l2 $l2 -M $M -m $m -g $g $b >>logs/run.log 2>>logs/error.log &"
-				done
+			for g in $graphs; do
+				eval "./main.py -l1 $l1 -l2 $l2 -M $M -m linear1 -g $g $b >>logs/run.log 2>>logs/error.log &"
 			done
 		done
 
-		sleep 90
+		sleep 60
 
 	done
 done
