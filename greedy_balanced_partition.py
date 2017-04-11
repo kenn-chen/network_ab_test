8cd3734f0269722fceb58efc05321adbfe91145b
Kenn <kenn.kikyoakane@gmail.com>
2017-04-11 19:36:45 +0900

    greedy balanced partition improvement

--- greedy_balanced_partition.py
+++ greedy_balanced_partition.py
@@ -5,11 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
 
+def prob(graph, node):
+	x = 1/graph.out_degree(node)
+	return x
+
 def get_weights(graph):
 	N = graph.number_of_nodes()
 	weights = [0] * N
 	for node in range(N):
-		weights[node] = 1 + sum(1/graph.out_degree(follower) for follower in graph.predecessors(node))
+		weights[node] = 1 + sum(prob(graph, follower) for follower in graph.predecessors(node))
 	weight_rank = [node for node, weight in sorted(enumerate(weights), key=lambda x: x[1], reverse=True)]
 	return weights, weight_rank
 
@@ -24,16 +28,24 @@ def joint_followees(graph, followees, node, weights):
 		followees[followee][1] = weights[followee]
 	return followees
 
-def majority_kernel(graph, mapping, node, k):
-	kernel_count = defaultdict(int)
+def remove_duplicate_weights(graph, node, kernel, total_weights):
+	n = graph.out_degree(node)
+	dup1 = sum(1/n for followee in graph.successors(node) if followee in kernel)
+	dup2 = sum(prob(graph, follower) for follower in graph.predecessors(node) if follower in kernel)
+	return total_weights - dup1 - dup2
+
+
+def majority_cluster(graph, mapping, clusters, node, cluster_size):
+	cluster_count = defaultdict(int)
 	for followee in graph.successors(node):
 		if followee in mapping:
-			kernel_id = mapping[followee]
-			kernel_count[kernel_id] += 1
-	if not kernel_count:
-		return random.randint(0, k-1)
+			cluster_id = mapping[followee]
+			if len(clusters[cluster_id]) < 2 * cluster_size:
+				cluster_count[cluster_id] += 1
+	if not cluster_count:
+		return random.randint(0, len(clusters)-1)
 	else:
-		return max(kernel_count, key=kernel_count.get)
+		return max(cluster_count, key=cluster_count.get)
 
 
 
@@ -42,15 +54,15 @@ def majority_vote(graph, kernels):
 	for i in range(len(kernels)):
 		for node in kernels[i]:
 			mapping[node] = i
-	k = len(kernels)
 	N = graph.number_of_nodes()
+	cluster_size = N / len(kernels)
 	clusters = [list(kernel) for kernel in kernels]
 	for node in range(N):
 		if node in mapping:
 			continue
-		kernel_id = majority_kernel(graph, mapping, node, k)
-		mapping[node] = kernel_id
-		clusters[kernel_id].append(node)
+		cluster_id = majority_cluster(graph, mapping, clusters, node, cluster_size)
+		mapping[node] = cluster_id
+		clusters[cluster_id].append(node)
 	return clusters
 
 def get_graph(graph_file):
@@ -58,41 +70,39 @@ def get_graph(graph_file):
 	return graph
 
 def get_kernels(graph):
-	weights, weight_rank = get_weights(graph)
 	N = graph.number_of_nodes()
+	weights, weight_rank = get_weights(graph)
 
-	iter_round = 200
+	iter_round = 100
 	cluster_size = N // iter_round
-	seed_node_range = int(N // 10)
+	seed_node_range = N // 10
 	kernels = []
 	selected_nodes = set()
 	for _ in range(iter_round):
 		kernel = set()
-		seed_node = weight_rank[random.randint(0, seed_node_range)]
+		seed_node = weight_rank[random.randint(0, N-1)]
 		if seed_node in selected_nodes:
 			continue
 		selected_nodes.add(seed_node)
 		kernel.add(seed_node)
 		followees = joint_followees(graph, None, seed_node, weights)
 
-		success = True
 		total_weights = weights[seed_node]
 		while total_weights < cluster_size:
 			found = False
 			ranked_followees = [node for node, _ in sorted(followees.items(), key=lambda x: x[1], reverse=True)]
 			for followee in ranked_followees:
-				if followee not in selected_nodes and followee not in kernels:
+				if followee not in selected_nodes and followee not in kernel:
 					found = True
 					kernel.add(followee)
-					selected_nodes.add(followee)
 					followees = joint_followees(graph, followees, followee, weights)
-					total_weights += weights[followee]
+					total_weights = remove_duplicate_weights(graph, followee, kernel, total_weights + weights[followee])
 					break
 			if not found:
-				success = False
 				break
-		if success:
+		if total_weights >= cluster_size:
 			kernels.append(kernel)
+			selected_nodes |= kernel
 	return kernels
 
 def clustering(graph):
